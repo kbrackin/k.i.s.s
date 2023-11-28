e577bbc4b1fa8e61c83567729acd240d163e58c3
Sean Worrell <spworrell@gmail.com>
2023-11-27 20:11:06 -0500

    start code with GPT

---
+++ server/index.js
@@ -0,0 +1,26 @@
+require("dotenv").config();
+const path = require("path");
+
+// Express setup
+const express = require("express");
+const app = express();
+
+//api add
+const routes = require("./routes");
+
+// add Json Support
+app.use(express.json());
+
+// add routes for apis
+app.use(routes);
+
+// host public react
+app.use(express.static(path.join(__dirname, "../client/dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/dist/index.html"));
+});
+
+// start express
+app.listen(3000, () => {
+  console.log("App listening on port 3000");
+});
