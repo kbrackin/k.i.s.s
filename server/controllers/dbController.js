b93225497457f0a169b1f1cb48c6f46488450915
Sean Worrell <spworrell@gmail.com>
2023-11-30 18:01:59 -0500

    api fixed

--- server/controllers/dbController.js
+++ server/controllers/dbController.js
@@ -1,29 +1,20 @@
-const express = require("express");
-const router = express.Router()
-
-const { user,issues } = require('../models')
+const { user, issues } = require("../models");
 
 module.exports = {
+  async putIssue(req, res) {
+    try {
+      const User = await user.create(req.body);
+      res.json(User);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
 
-    async putIssue(req, res) {
-        try {
-          const user = await user.create(req.body)
-          res.json(user);
-        } catch (err) {
-          res.status(500).json(err);
-        }
-      },
-
-      
-
-      async routeTest(req, res) {
-        try {
-          res.send("Test Worked")
-        } catch (err) {
-          res.status(500).json(err);
-        }
-      },
-
- }
-
-    
+  async routeTest(req, res) {
+    try {
+      res.send("Test Worked");
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+};
