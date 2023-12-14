4f63d277c62b1e0ca3ae1dcbbc5aef89e958d871
kbrackin <135669996+kbrackin@users.noreply.github.com>
2023-12-14 01:23:19 -0500

    function to update resolved status

--- client/src/components/requestor.jsx
+++ client/src/components/requestor.jsx
@@ -1,48 +1,52 @@
 import { Col, Row, Button, Form } from "react-bootstrap";
 import axios from "axios";
 import Auth from "../util/auth";
-import { useState } from "react";
+import React, { useState } from "react";
+
+
 
 function RequestorList(props) {
-  const [resolved,setResolved] = useState(false)
-  console.log(props)
-  const resolve = (event, issueID) => {
-    console.log("IssueId",issueID,"value",event.target.value)
-    // if(event.target.value){
-    //  
-    // }
-    // else{
-    //   event.target.value = true
-    // }
-    // if(document.getElementById(`custom-switch-${issue.id}`).value === "off") {
-      if(event.target.value){
-      axios.put(`/api/issue/${userId}/${issueID}`, {
-        resolve:false,
-     
+  
+  const [issueResolved, setIssueResolved] = useState({ resolved: false })
+  
+  const handleChange = (e, issueID, key) => {
+    const { checked } = e.target
+    const resolved = issueResolved
+    console.log("IssueId", issueID, "value",checked)
+
+    if (checked) {
+      setIssueResolved({
+        resolved: true
+      });
+      axios.put(`/api/issue/${key}/${issueID}`, {
+        resolved: true,
+
         headers: {
           Authorization: `Bearer ${Auth.getToken()}`,
         },
-    
+
       })
-      .then(function (response) {
-        return response
-      })
-      .catch(function (error) {
-        return error
-      })
+        .then(function (response) {
+          return response
+        })
+        .catch(function (error) {
+          return error
+        })
     } else {
-      axios.put(`/api/issue/${userId}/${issueID}`, {
+      axios.put(`/api/issue/${key}/${issueID}`, {
         resolved: true,
         headers: {
           Authorization: `Bearer ${Auth.getToken()}`,
-      }
+        }
       }).then(function (response) {
         return response
       })
-      .catch(function (error) {
-        return error
-      })
-  }}
+        .catch(function (error) {
+          return error
+        })
+    }
+  }
+
   return (
     <>
       <p>
@@ -51,7 +55,7 @@ function RequestorList(props) {
       <p>
         <strong>Email:</strong> {props.email}
       </p>
-      <p>
+      <div>
         <Row>
           <Col className="col-lg-1">
             <tr>
@@ -62,20 +66,19 @@ function RequestorList(props) {
             <tr>
               <Col>
                 {props.issues.map((issue) => (
-                  <div key={issue.id}>
-                    <td id="tablecontent">Issue: {issue.issues}</td>
+                  <div key={issue._id}>
+                    <td id="tablecontent-left">Issue: {issue.issues}</td>
                     <td id="tablecontent">
                       {" "}
                       Is Resolved:{" "}
-                      <Form>
-                        <Form.Check // prettier-ignore
-                          type="switch"
-                          id={`custom-switch-${issue._id}`}
-                          label="Check this switch"
-                          value= {issue.resolved}
-                          onClick={(event) => resolve(event,issue._id)}
-                        />
-                      </Form>
+                      <Form.Check // prettier-ignore
+                        type="checkbox"
+                        id={issue._id}
+                        label=""
+                        value={issue.issues.resolved}
+                        onChange={handleChange}
+                      />
+
                     </td>
                   </div>
                 ))}
@@ -83,16 +86,17 @@ function RequestorList(props) {
             </tr>
           </table>
           <Col>
-          <a href={`mailto:${props.email}`}>
-            <Button>Contact User</Button>
+            <a href={`mailto:${props.email}`}>
+              <Button>Contact User</Button>
             </a>
           </Col>
         </Row>
-      </p>
+      </div>
     </>
   );
-;
+  ;
 }
 export default RequestorList;
 
 //{issue.resolved.toString()
+//(e) => resolve(e, issue._id)
